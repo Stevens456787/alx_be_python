b4d561fa834f4987d4843a434c4f53fa104e93ff
Stevens456787 <stevenstrudient@gmail.com>
2024-12-25 18:09:21 +0300

    Delete control_flow directory

--- control_flow/multiplication_table.py
+++
@@ -1,8 +0,0 @@
-# multiplication_table.py
-
-# Prompt user for input
-number = int(input("Enter a number to see its multiplication table: "))
-
-# Generate and print the multiplication table
-for i in range(1, 11):
-    print(f"{number} * {i} = {number * i}")
