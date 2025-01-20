7b4ed57b51c8f3ba399c3eb4252198a250ccafe1
stevens456787 <stevenstrudient@gmail.com>
2025-01-20 14:50:53 +0300

    update the calculator.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,6 +1,8 @@
 class Calculator:
+    calculation_type = "Arithmetic Operations"
     def add(a, b):
         return a + b
     
-    def multiply(a, b):
+    def multiply(cls,a, b):
+        print(f"Calculation type:cls.calculation_type")
         return a * b
